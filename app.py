ecdc8a3c6a2574f036594b007e59e87e9e889f54
jplepinsk1 <seuemail@gmail.com>
2026-06-02 15:27:49 -0300

    atualiza modelo do chat para gemini-3.1-flash-lite

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def get_user_chat():
         print(f"Criando novo chat Gemini para session_id: {session_id}")
         try:
             chat_session = client.chats.create(
-                model="gemini-2.5-flash", # Certifique-se que este modelo suporta chat contínuo
+                model="gemini-3.1-flash-lite", # Certifique-se que este modelo suporta chat contínuo
                 config=types.GenerateContentConfig(system_instruction=instrucoes)
             )
             active_chats[session_id] = chat_session
